2e90e1487ac06369917ba886a8e4b815c0582ed8
DevMonster <dmcho84@gmail.com>
2018-04-25 05:09:23 +0900

    gogo

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ app.use((ctx, next) => {
     });
 
     ctx.response.set('Access-Control-Allow-Credentials', true);
-    // ctx.response.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    ctx.response.set('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
     ctx.response.set('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
     
 
